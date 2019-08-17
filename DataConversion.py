5fc898f59a260db3f29a625ef59c8f45504cfb90
Martin Metodiev <metodiem@uni.coventry.ac.uk>
2019-08-18 00:04:47 +0100

    Final version of the data manipulation script

--- DataConversion.py
+++ DataConversion.py
@@ -4,38 +4,43 @@ import time
 import re
 import ipaddress
 
-path = "D:\\University\\Master\\M102CEM - Project\\Code\\Data\\Original_CSV\\16-09-23.csv"
 
-#time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(epoch))
-
-
+#Convert the epoch time to just hours and minutes
 def toTime(epoch):
-    return time.strftime("%H%M", time.localtime(int(epoch)))
+    try:
+        return time.strftime("%H%M", time.localtime(int(epoch)))
+    except:
+        return epoch
 
+#Convert a MAC address to a 64bit integer
 def mac_to_int(mac):
     res = re.match('^((?:(?:[0-9a-f]{2}):){5}[0-9a-f]{2})$', mac.lower())
     if res is None:
-        raise ValueError('invalid mac address')
+        return mac
     return int(res.group(0).replace(':', ''), 16)
 
+#Convert an IP address to a 64bit integer
 def ip_to_int(ip):
-    return int(ipaddress.IPv4Address(ip))
+    try:
+        return int(ipaddress.IPv4Address(ip))
+    except:
+        return ip
 
-path_to_write = "D:\\University\\Master\\M102CEM - Project\\Code\\Converted.csv"
 
-newCSV = []
+if __name__ == '__main__':
 
-with open(path) as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    count = 0;
-    for row in readCSV:
-        if count != 0:
-            newCSV.append([toTime(row[1]),row[2],mac_to_int(row[3]),mac_to_int(row[4]),ip_to_int(row[5]),ip_to_int(row[6]),row[7],row[8],row[9],row[10]])
-        else:
-            newCSV.append(row[1:])
-        count += 1
+    path = "D:\\University\\Master\\M102CEM - Project\\Code\\Data\\Original_CSV\\16-09-23.csv"
+    path_to_write = "D:\\University\\Master\\M102CEM - Project\\Code\\Converted.csv"
+    newCSV = []
 
-with open(path_to_write, mode='w', newline='') as CSV:
-    writer = csv.writer(CSV)
-    for row in newCSV:
-        writer.writerow(row)
+    #Open and read the CSV file
+    with open(path) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            newCSV.append([toTime(row[1]),row[2],row[7],row[8],row[9],row[10]]) #Add only the data we need to a list
+    
+    #Open the new file and write the data to it
+    with open(path_to_write, mode='w', newline='') as CSV:
+        writer = csv.writer(CSV)
+        for row in newCSV:
+            writer.writerow(row)
